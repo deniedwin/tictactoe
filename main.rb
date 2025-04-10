400ae5f39a43debafb707c434ec4704433f8d184
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-04-10 17:08:16 +0200

    Made small updates to the game

--- main.rb
+++ main.rb
@@ -1,8 +1,26 @@
 class Board
-  def intro_message
-    puts 'board is empty'
+  @@board_state = [1,2,3,4,5,6,7,8,9]
+  def draw_board
+    col_seperator = '|'
+    row_seperator = '--+--+--'
+    @@board_state.each do |i| 
+      print "#{i} #{col_seperator}"
+      if i%3 == 0
+        puts "\n"
+        puts row_seperator
+      end
+    end
   end
 end
 
-new_board = Board.new
-new_board.intro_message
+class Player
+  def intro_player
+    puts 'put new player'
+  end
+end
+
+main_board = Board.new
+main_board.draw_board
+
+# player1 = Player.new
+# player2 = Player.new
