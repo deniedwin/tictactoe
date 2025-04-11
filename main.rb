83176feef298c6d6f4819c9611beb8d6f8d530ca
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-04-11 13:28:16 +0200

    Made major changes to file

--- main.rb
+++ main.rb
@@ -1,26 +1,71 @@
-class Board
-  @@board_state = [1,2,3,4,5,6,7,8,9]
-  def draw_board
-    col_seperator = '|'
-    row_seperator = '--+--+--'
-    @@board_state.each do |i| 
-      print "#{i} #{col_seperator}"
-      if i%3 == 0
-        puts "\n"
-        puts row_seperator
+class Game
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @won = false
+  end
+
+  def start_game
+    game_board = Board.new
+    (1..9).each do |i|
+      game_board.draw_board
+
+      if i%2 == 0
+        @player1.make_move(game_board, 'x')
+      else
+        @player2.make_move(game_board, 'o')
       end
     end
   end
 end
 
+class Board
+  @@board_state = [1,2,3,4,5,6,7,8,9]
+
+  def draw_board
+    col_seperator = '|'
+    row_seperator = '--+--+--'
+    @@board_state.each_with_index do |i,idx|
+      print "#{i} #{col_seperator}"
+      if idx == 2 || idx == 5 || idx == 8
+        puts "\n"
+        puts row_seperator
+      end
+    end
+  end
+
+  def valid_move?(position)
+    if position.between?(1,9) && @@board_state[position-1].is_a?(Integer)
+      true
+    else
+      puts 'wrong move, try again!'
+      false
+    end
+  end
+
+  def update_position(position, marker)
+    @@board_state.each_with_index do |j,idx|
+      if j == position
+        @@board_state[idx] = marker
+      end
+    end
+  end
+
+end
+
 class Player
-  def intro_player
-    puts 'put new player'
+  def make_move(game_board, marker)
+    puts "player '#{marker}' choose number form 1 to 9:"
+    while true
+      selection = gets
+      selection = selection.to_i
+      if game_board.valid_move?(selection) == true
+        game_board.update_position(selection, marker)
+        break
+      end
+    end
   end
 end
 
-main_board = Board.new
-main_board.draw_board
-
-# player1 = Player.new
-# player2 = Player.new
+tictactoe = Game.new(Player.new, Player.new)
+tictactoe.start_game
