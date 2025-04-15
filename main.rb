451044d08a9297fa674ca0221faa220299f21d92
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-04-15 16:59:34 +0200

    Update valid_move method

--- main.rb
+++ main.rb
@@ -1,47 +1,47 @@
 class Game
-  WINNER_LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-  #find way to add this to check for winners
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @board = Board.new
   end
 
-  def start_game
-    game_board = Board.new
-    (1..9).each do |i|
-      game_board.draw_board
+  def play_turn(player)
+    @board.draw_board
+    player.make_move(@board)
+  end
 
-      if i.even?
-        @player1.make_move(game_board, player1.marker)
-      else
-        @player2.make_move(game_board, player2.marker)
-      end
-      
-      if Board.has_won?
-        game_board.draw_board
-        puts 'stop game!'
-        puts "the winner is: #{} player-#{player1.marker}"
-        break
-      elsif Board.has_draw?
-        game_board.draw_board
-        puts 'its a draw!'
-        break
-      end
+  def announce_result(winner)
+    @board.draw_board
+    if winner
+      puts "the winner is #{winner}"
+    else
+      puts "its a draw"
     end
   end
 
-  def self.winner_lines
-    WINNER_LINES
+  def start_game
+    @board = Board.new
+    current_player = @player1
+    loop do
+      play_turn(current_player)
+      winner = @board.has_won?([@player1, @player2])
+      break announce_result(winner) if winner || @board.has_draw?
+      current_player = switch_player(current_player)
+    end
   end
 end
 
 class Board
-  @@board_state = [1,2,3,4,5,6,7,8,9]
+  WINNER_LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+  
+  def initialize
+    @state = (1..9).to_a
+  end
 
   def draw_board
     col_seperator = '|'
     row_seperator = '--+--+--'
-    @@board_state.each_with_index do |i,idx|
+    @state.each_with_index do |i,idx|
       print "#{i} #{col_seperator}"
       if idx == 2 || idx == 5 || idx == 8
         puts "\n"
@@ -51,55 +51,52 @@ class Board
   end
 
   def valid_move?(position)
-    if position.between?(1,9) && @@board_state[position-1].is_a?(Integer)
-      true
-    else
-      puts 'wrong move, try again!'
-      false
-    end
+    position.between?(1,9) && @state[position-1].is_a?(Integer)
   end
 
   def update_position(position, marker)
-    @@board_state.each_with_index do |j,idx|
+    @state.each_with_index do |j,idx|
       if j == position
-        @@board_state[idx] = marker
+        @state[idx] = marker
       end
     end
   end
 
-  def self.has_won?
-    Game.winner_lines.each do |i_item|
-      if @@board_state[i_item[0]-1] == @@board_state[i_item[1]-1] && @@board_state[i_item[0]-1] == @@board_state[i_item[2]-1]
-        return true
+  def has_won?(players)
+    WINNER_LINES.each do |i_item|
+      if @state[i_item[0]-1] == @state[i_item[1]-1] && @state[i_item[0]-1] == @state[i_item[2]-1]
+        return players.find {|player| player.marker == (i_item[0]-1)}
       end
     end
     return false
   end
 
-  def self.has_draw?
-    @@board_state.all? {|cell| cell.is_a?(String)}
+  def has_draw?
+    @state.all? {|cell| cell.is_a?(String)}
   end
 end
 
 class Player
-  def make_move(game_board, marker)
-    puts "player '#{marker}' choose number form 1 to 9:"
-    while true
-      selection = gets
-      selection = selection.to_i
-      if game_board.valid_move?(selection) == true
-        game_board.update_position(selection, marker)
-        break
-      end
-    end
-  end
 
   def initialize(id, marker)
     @id = id
     @marker = marker
   end
+
   attr_reader :id, :marker
+
+  def make_move(game_board) #UPDATE THIS!
+    puts "player '#{@marker}' choose number form 1 to 9:"
+    while true
+      selection = gets
+      selection = selection.to_i
+      if game_board.valid_move?(selection) == true
+        game_board.update_position(selection, @marker)
+        break
+      end
+    end
+  end
 end
 
-tictactoe = Game.new(Player.new(111,'x'), Player.new(222,'o'))
+tictactoe = Game.new(Player.new(1,'x'), Player.new(2,'o'))
 tictactoe.start_game
