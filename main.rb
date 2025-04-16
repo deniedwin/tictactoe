52af4eac865cab3b6d1bb6502f20e33410518f7b
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-04-16 13:15:35 +0200

    Added rubocop to the project

--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# this class is in charge of the whole game
 class Game
   def initialize(player1, player2)
     @player1 = player1
@@ -13,27 +16,32 @@ class Game
   def announce_result(winner)
     @board.draw_board
     if winner
-      puts "the winner is #{winner}"
+      puts "the winner is = #{winner}"
     else
-      puts "its a draw"
+      puts 'its a draw'
     end
   end
 
   def start_game
-    @board = Board.new
     current_player = @player1
     loop do
       play_turn(current_player)
-      winner = @board.has_won?([@player1, @player2])
-      break announce_result(winner) if winner || @board.has_draw?
+      winner = @board.won?([@player1, @player2])
+      break announce_result(winner) if winner || @board.draw?
+
       current_player = switch_player(current_player)
     end
   end
+
+  def switch_player(current_player)
+    current_player == @player1 ? @player2 : @player1
+  end
 end
 
+# this class is in charge of the state of the board while it update
 class Board
-  WINNER_LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-  
+  WINNER_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
+
   def initialize
     @state = (1..9).to_a
   end
@@ -41,9 +49,9 @@ class Board
   def draw_board
     col_seperator = '|'
     row_seperator = '--+--+--'
-    @state.each_with_index do |i,idx|
+    @state.each_with_index do |i, idx|
       print "#{i} #{col_seperator}"
-      if idx == 2 || idx == 5 || idx == 8
+      if [2, 5, 8].include?(idx)
         puts "\n"
         puts row_seperator
       end
@@ -51,52 +59,62 @@ class Board
   end
 
   def valid_move?(position)
-    position.between?(1,9) && @state[position-1].is_a?(Integer)
+    position.between?(1, 9) && @state[position - 1].is_a?(Integer)
   end
 
-  def update_position(position, marker)
-    @state.each_with_index do |j,idx|
-      if j == position
-        @state[idx] = marker
+  def mark_cell(position, player)
+    @state[position - 1] = player.marker
+  end
+
+  def won?(players)
+    WINNER_LINES.each do |line|
+      first = @state[line[0] - 1]
+      if line.all? { |pos| @state[pos - 1] == first } && first.is_a?(String)
+        return players.find { |player| player.marker == first }
       end
     end
+    false
   end
 
-  def has_won?(players)
-    WINNER_LINES.each do |i_item|
-      if @state[i_item[0]-1] == @state[i_item[1]-1] && @state[i_item[0]-1] == @state[i_item[2]-1]
-        return players.find {|player| player.marker == (i_item[0]-1)}
-      end
-    end
-    return false
-  end
-
-  def has_draw?
-    @state.all? {|cell| cell.is_a?(String)}
+  def draw?
+    @state.all? { |cell| cell.is_a?(String) }
   end
 end
 
+# this class stores player variables and makes the player do a move
 class Player
-
   def initialize(id, marker)
     @id = id
     @marker = marker
   end
 
+  def to_s
+    "Player: #{@id} with mark: #{@marker}"
+  end
+
   attr_reader :id, :marker
 
-  def make_move(game_board) #UPDATE THIS!
-    puts "player '#{@marker}' choose number form 1 to 9:"
-    while true
-      selection = gets
-      selection = selection.to_i
-      if game_board.valid_move?(selection) == true
-        game_board.update_position(selection, @marker)
+  def make_move(board)
+    puts "Player-#{@marker} choose a number:"
+    loop do
+      position = gets.to_i
+      if board.valid_move?(position)
+        board.mark_cell(position, self)
         break
+      else
+        puts 'invalid move, try again!'
       end
     end
   end
 end
 
-tictactoe = Game.new(Player.new(1,'x'), Player.new(2,'o'))
-tictactoe.start_game
+# this class functions as running fuction, meaning that it is only to run the game
+class GameRunner
+  def self.run
+    player1 = Player.new(1, 'x')
+    player2 = Player.new(2, 'o')
+    Game.new(player1, player2).start_game
+  end
+end
+
+GameRunner.run
